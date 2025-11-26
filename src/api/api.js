54a10162799dbe50a15a71e7e9cd561cce597328
kalyaniprinki <kalyaniprinki@gmail.com>
2025-11-27 00:00:59 +0530

    first commit18

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,6 @@
-// src/api/api.js
 import axios from "axios";
 
-// Use deployed backend URL directly
-const API_URL = "https://project-yjqr.onrender.com/api";
-
+export const API_URL = "https://project-yjqr.onrender.com/api"; // REQUIRED
 const api = axios.create({
   baseURL: API_URL,
   headers: {
@@ -11,30 +8,12 @@ const api = axios.create({
   },
 });
 
-// Automatically attach token from localStorage to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Optional: globally handle responses / errors
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response) {
-      // Backend returned an error response
-      console.error("API Error:", error.response.status, error.response.data);
-    } else {
-      console.error("API Network / Unknown Error:", error.message);
-    }
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 export default api;
