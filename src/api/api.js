3453eceeb74a3c87b2001a685a15254364df8938
kalyaniprinki <kalyaniprinki@gmail.com>
2025-11-26 23:18:21 +0530

    first commit8

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const API_URL = "http://localhost:5000/api";
-export const API_URL = "https://kiosk-frontend-amber.vercel.app/api";
+export const API_URL = "https://kiosk-frontend-amber.vercel.app";
 
 const api = axios.create({
   baseURL: API_URL,
