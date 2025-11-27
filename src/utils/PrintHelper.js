0c8b45939b47e99ef42887b473228d364ed27e15
kalyaniprinki <kalyaniprinki@gmail.com>
2025-11-27 19:09:00 +0530

    update commit3

--- src/utils/PrintHelper.js
+++ src/utils/PrintHelper.js
@@ -1,12 +1,23 @@
 import axios from "axios";
-
-export default async function sendToLocalPrinter(filePath) {
+export default async function sendToLocalPrinter(url) {
   try {
-    const res = await axios.post("http://localhost:9100/print", {
-      filePath,
+    const res = await axios.post("http://localhost:9100/download-and-print", {
+      url
     });
     return res.data;
   } catch (err) {
     return { error: true, message: err.message };
   }
 }
+
+
+// export default async function sendToLocalPrinter(filePath) {
+//   try {
+//     const res = await axios.post("http://localhost:9100/print", {
+//       filePath,
+//     });
+//     return res.data;
+//   } catch (err) {
+//     return { error: true, message: err.message };
+//   }
+// }
